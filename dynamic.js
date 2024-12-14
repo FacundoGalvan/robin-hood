e41d837db64082b231f804f0fa529510973cc27e
FacundoGalvan <facugalvan097@gmail.com>
2024-12-14 12:11:04 +1000

    first commit

---
+++ dynamic.js
@@ -0,0 +1,17 @@
+//metodo length para cambiar el ultimo elemento del array 
+
+let perros = ["Omy", "Hija", "India"]
+
+perros[perros.length - 1] = "Musa"; 
+
+console.log(perros);
+
+// ahora el ultimo elemento del array es Musa.
+
+
+//otro metodo es push que agrega un valor en el ultimo elemento del array.
+
+perros.push("Otto");
+
+console.log(perros);
+
---
+++ practica.js
@@ -0,0 +1,14 @@
+let count = 3; // DECLARACION DE VARIABLE NUMERICA. 
+console.log(count + 1); // SUMO 1 AL NUMERO DECLARADO.
+console.log(count * 1); // SE PUEDE HACER TAMBIEN CON DIVISION, MULTIPLICACION Y RESTA.
+
+//Bracket notation 
+// es para acceder al valor 
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0])
+
+//mutation
+// es para cambiar el valor de un array.
+rows[2] = 10; 
+// ahora el valor [2] del array pasa a valer 10.
+
